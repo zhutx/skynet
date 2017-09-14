13a5193c8cbd93431583f3d870eece11d2f8490c
zhutx <16530202@qq.com>
2017-09-14 15:58:23 +0800

    提交修改

--- skynet-service/src/main/java/com/moredian/fishnet/cache/RedisConfig.java
+++ skynet-service/src/main/java/com/moredian/fishnet/cache/RedisConfig.java
@@ -1,4 +1,4 @@
-package com.moredian.fishnet.device.cache;
+package com.moredian.fishnet.cache;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
